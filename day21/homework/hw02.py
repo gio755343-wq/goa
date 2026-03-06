24ac1f37d909506ca29f93f33d6c6325be670a6b
gio755343-wq <gio755343@gmail.com>
2026-03-06 18:21:42 +0400

    რაც გინდა ის დაწერ

---
+++ day21/homework/hw02.py
@@ -0,0 +1,25 @@
+# 2)დაწერეთ ყველა ფუნქციაზე 3-3 მაგალითი
+
+print("Hello")
+
+print("Gamarjoba")
+
+print(10)
+
+name = input("შეიყვანე შენი სახელი: ")
+
+age = input("შეიყვანე შენი ასაკი: ")
+
+city = input("შეიყვანე ქალაქი: ")
+
+name = input("შეიყვანე შენი სახელი: ")
+
+age = input("შეიყვანე შენი ასაკი: ")
+
+city = input("შეიყვანე ქალაქი: ")
+
+text = str(10)
+
+name = str(25)
+
+word = str(True)
